3b0d457aa1cf34d2a77413bc44befc2c841202f7
Nham <nguyenducnhamndn@gmail.com>
2022-09-08 23:23:30 +0700

    updated

--- Js/project.js
+++ Js/project.js
@@ -1,13 +1,14 @@
 //zoom img on scroll{}
 
-
 $(window).scroll(function () {
   let scroll = $(window).scrollTop();
   let width = $(window).width();
   if (scroll >= 196 && width >= 1024) {
     $(".project--nav").addClass("fixed");
+    $(".project--nav").addClass("justify-content-center");
   } else {
     $(".project--nav").removeClass("fixed");
+    $(".project--nav").removeClass("justify-content-center");
   }
 });
 $(".project--nav a").hover(
