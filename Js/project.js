28dd84908c2df2e8fa9de42185fd9a6f95745f16
Nham <nguyenducnhamndn@gmail.com>
2022-09-09 09:03:58 +0700

    update

--- Js/project.js
+++ Js/project.js
@@ -11,11 +11,33 @@ $(window).scroll(function () {
     $(".project--nav").removeClass("justify-content-center");
   }
 });
-$(".project--nav a").hover(
-  function () {
-    $(this).addClass("active");
-  },
-  function () {
-    $(this).removeClass("active");
-  }
-);
+// $(".project--nav a").mousemove(function () {
+//   $(this).css("background-color", "green");
+// });
+
+// $(".project--nav a").mouseout(function () {
+//   $(this).css("background-color", "orange");
+// });
+// $(".project--nav a").click(console.log(".project--nav a"));
+// Get the container element
+// var btnContainer = document.getElementsByClassName("project--nav");
+// console.log(btnContainer);
+// // Get all buttons with class="btn" inside the container
+// // var btns = btnContainer.getElementsByClassName("btn");
+
+// // Loop through the buttons and add the active class to the btnHovers/clicked button
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function () {
+//     var btnHovers = document.getElementsByClassName("active");
+//     btnHovers[0].className = btnHovers[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
+const btnHovers = document.querySelectorAll(".project--nav__btn");
+console.log(btnHovers);
+for (let i = 0; i < btnHovers.length; i++) {
+  btnHovers[i].addEventListener("click", () => {
+    btnHovers[0].className = btnHovers[0].className.replace(" active", "");
+    btnHovers[i].className += " active";
+  });
+}
