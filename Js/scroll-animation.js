aadd7b68bdfde24fdb36daa9c169f49f361dca83
Nham <nguyenducnhamndn@gmail.com>
2022-09-19 23:46:36 +0700

    update 19/9

--- Js/scroll-animation.js
+++ Js/scroll-animation.js
@@ -43,11 +43,11 @@ function toContact(params) {
 
 scrollbar.setPosition(0, 0);
 scrollbar.track.xAxis.element.remove();
-const header_setTop = $(".header-setTop").offset().top - 100; // distance top
 
 function listener(status) {
   let scroll = scrollbar.offset.y;
   let width = window.innerWidth;
+  const header_setTop = $(".header-setTop").offset().top - 100; // distance top
   if (width > 1200) {
     if (scroll <= 200) {
       $("header").css("backgroundColor", "transparent");
@@ -72,41 +72,39 @@ function listener(status) {
   } else {
     $("header").css("backgroundColor", "white");
   }
-    if ($("#detailsNews").attr("id")) {
-      const overview = document.getElementById("overview").offsetTop - 200;
-      const locations = document.getElementById("location").offsetTop - 200;
-      const utilities = document.getElementById("utilities").offsetTop - 200;
-      const perspective =
-        document.getElementById("perspective").offsetTop - 200;
-      const contact = document.getElementById("contact").offsetTop - 200;
-    
+  if ($("#detailsNews").attr("id")) {
+    const overview = document.getElementById("overview").offsetTop - 200;
+    const locations = document.getElementById("location").offsetTop - 200;
+    const utilities = document.getElementById("utilities").offsetTop - 200;
+    const perspective = document.getElementById("perspective").offsetTop - 200;
+    const contact = document.getElementById("contact").offsetTop - 200;
 
-      if (scroll >= overview && scroll < locations) {
-        $(".btn_overview").addClass("active");
-      } else {
-        $(".btn_overview").removeClass("active");
-      }
-      if (scroll >= locations && scroll < utilities) {
-        $(".btn_location").addClass("active");
-      } else {
-        $(".btn_location").removeClass("active");
-      }
-      if (scroll >= utilities && scroll < perspective) {
-        $(".btn_utilities").addClass("active");
-      } else {
-        $(".btn_utilities").removeClass("active");
-      }
-      if (scroll >= perspective && scroll < contact) {
-        $(".btn_perspective").addClass("active");
-      } else {
-        $(".btn_perspective").removeClass("active");
-      }
-      if (scroll > contact) {
-        $(".btn_contact").addClass("active");
-      } else {
-        $(".btn_contact").removeClass("active");
-      }
+    if (scroll >= overview && scroll < locations) {
+      $(".btn_overview").addClass("active");
+    } else {
+      $(".btn_overview").removeClass("active");
     }
+    if (scroll >= locations && scroll < utilities) {
+      $(".btn_location").addClass("active");
+    } else {
+      $(".btn_location").removeClass("active");
+    }
+    if (scroll >= utilities && scroll < perspective) {
+      $(".btn_utilities").addClass("active");
+    } else {
+      $(".btn_utilities").removeClass("active");
+    }
+    if (scroll >= perspective && scroll < contact) {
+      $(".btn_perspective").addClass("active");
+    } else {
+      $(".btn_perspective").removeClass("active");
+    }
+    if (scroll > contact) {
+      $(".btn_contact").addClass("active");
+    } else {
+      $(".btn_contact").removeClass("active");
+    }
+  }
 }
 scrollbar.addListener(listener);
 
